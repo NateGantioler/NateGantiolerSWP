4995a333985e35f00f1c5abc05bbee17e76e7147
NateGantioler <nate.gantioler@gmail.com>
2022-09-28 10:47:23 +0200

    Wurfelbeispiel

--- src/WuerfelSpiel.java
+++ src/WuerfelSpiel.java
@@ -1,4 +1,21 @@
+import com.sun.tools.javac.Main;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class WuerfelSpiel
 {
-    
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int Wurfel = 0;
+        Wurfel = scanner.nextInt();
+        for(int i = 0; i < 100; i++)
+        {
+            Wurfel = random.nextInt(1, 6);
+            System.out.print(Wurfel);
+        }
+    }
 }
