0b7d2235aca76fa42553970d65255df8b5c0fe1f
NateGantioler <nate.gantioler@gmail.com>
2022-09-28 11:35:07 +0200

    Wurfelspiel fertig

--- src/WuerfelSpiel.java
+++ src/WuerfelSpiel.java
@@ -9,13 +9,52 @@ public class WuerfelSpiel
     {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        int dice = 0;
+        int diceTotal1 = 0;
+        int diceTotal2 = 0;
 
-        int Wurfel = 0;
-        Wurfel = scanner.nextInt();
-        for(int i = 0; i < 100; i++)
+        //User
+        System.out.print("User: \n------------------------------------");
+        for(int i = 0; i < 6; i++)
         {
-            Wurfel = random.nextInt(1, 6);
-            System.out.print(Wurfel);
+            scanner.nextLine();
+            dice = random.nextInt(1, 7);
+            System.out.print(dice);
+            diceTotal1 += dice;
+        }
+
+
+        //Computer
+        scanner.nextLine();
+        System.out.println("Computer: \n------------------------------------");
+        for(int i = 0; i < 5; i++)
+        {
+            dice = random.nextInt(1, 7);
+            System.out.print(dice + " - ");
+            diceTotal2 += dice;
+        }
+        dice = random.nextInt(1, 7);
+        System.out.println(dice);
+        diceTotal2 += dice;
+
+
+        //Result
+        scanner.nextLine();
+        System.out.print("User dice total    vs    Computer dice total");
+        scanner.nextLine();
+        System.out.println("      " + diceTotal1 + "                        " + diceTotal2);
+        System.out.println("------------------------------------");
+        if(diceTotal1 < diceTotal2)
+        {
+            System.out.println("Computer Won!");
+        }
+        else if(diceTotal1 > diceTotal2)
+        {
+            System.out.println("User Won!");
+        }
+        else
+        {
+            System.out.println("Draw!");
         }
     }
 }
